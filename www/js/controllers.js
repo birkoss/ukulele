d6d6db2824d4213ce94d7a527546854d8cb74bc2
birkoss <admin@birkoss.com>
2017-01-30 06:25:05 -0500

    Adding Major scales and Chords

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -5,6 +5,8 @@ ukuleleApp.controller('ChordsList', function($scope, $ionicModal, $ionicSideMenu
     $scope.chords = ChordsFactory.get();
     $scope.chord_types = ChordTypesFactory.get();
 
+    $scope.scales = ['A', 'A#/Bb', 'B', 'C', 'C#/Db', 'D', 'D#/Eb', 'E', 'F', 'F#/Gb', 'G', 'G#/Ab'];
+
     /* Prepare the chord detail modal */
     $ionicModal.fromTemplateUrl('chord-detail.html', {
         scope: $scope,
@@ -16,6 +18,30 @@ ukuleleApp.controller('ChordsList', function($scope, $ionicModal, $ionicSideMenu
     /* Show/Hide chord detail in a modal */
     $scope.showChord = function(chord) {
         $scope.current_chord = chord;
+
+        $scope.current_scale = [];
+        var scale_modifier = [2, 2, 1, 2, 2, 2, 1];
+        $scope.scale_parts = [1, 3, 5];
+
+        var scales = $scope.scales.concat($scope.scales);
+        for (var i=0; i<scales.length; i++) {
+            if (scales[i] == chord.name || (scales[i].indexOf('/' + chord.name) >= 0 || scales[i].indexOf(chord.name + '/') >= 0)) {
+                $scope.current_scale.push(chord.name);
+                for (var j=0; j<scale_modifier.length; j++) {
+                    i += scale_modifier[j];
+                    var inline_scales = scales[i].split('/');
+                    var scale = inline_scales[0];
+                    if (inline_scales.length > 1) {
+                        // Choose the best name
+                        if ($scope.current_scale[$scope.current_scale.length-1].substr(0, 1) == scale.substr(0, 1)) {
+                            scale = inline_scales[1];
+                        }
+                    }
+                    $scope.current_scale.push( scale );
+                }
+                break;
+            }
+        }
 
         $scope.modal_chord_detail.show();
     };
--- www/js/factories.js
+++ www/js/factories.js
@@ -3,18 +3,164 @@ ukuleleApp.factory('ChordsFactory', function() {
         get: function() {
             var chords = [];
 
-            chords.push({'name': 'A', 'french_name': 'LA'});
-            chords.push({'name': 'A# / Bb', 'french_name': 'LA # / SI b'});
-            chords.push({'name': 'B', 'french_name': 'SI'});
-            chords.push({'name': 'C', 'french_name': 'DO'});
-            chords.push({'name': 'C# / Db', 'french_name': 'DO # / RÉ b'});
-            chords.push({'name': 'D', 'french_name': 'RÉ'});
-            chords.push({'name': 'D# / Eb', 'french_name': 'RÉ # / MI b'});
-            chords.push({'name': 'E', 'french_name': 'MI'});
-            chords.push({'name': 'F', 'french_name': 'Fa'});
-            chords.push({'name': 'F# / Gb', 'french_name': 'Fa # / SOL b'});
-            chords.push({'name': 'G', 'french_name': 'Sol'});
-            chords.push({'name': 'G# / Ab', 'french_name': 'SOL # / LA b'});
+            chords.push({
+                'name': 'A',
+                'french_name': 'LA',
+                'chords': {
+                    'Major': [{
+                        'Fingers': {
+                            'C1': '1',
+                            'G2': '2',
+                        }
+                    }]
+                }
+            });
+            chords.push({
+                'name': 'Bb', 
+                'french_name': 'SI b',
+                'chords': {
+                    'Major': [{
+                        'Fingers': {
+                            'A1': '1',
+                            'E1': '1',
+                            'C2': '2',
+                            'G3': '3',
+                        }
+                    }]
+                }
+            });
+            chords.push({
+                'name': 'B', 
+                'french_name': 'SI',
+                'chords': {
+                    'Major': [{
+                        'Fingers': {
+                            'A2': '1',
+                            'E2': '1',
+                            'C3': '2',
+                            'G4': '3',
+                        }
+                    }]
+                }
+            });
+            chords.push({
+                'name': 'C', 
+                'french_name': 'DO',
+                'chords': {
+                    'Major': [{
+                        'Fingers': {
+                            'A3': '3',
+                        }
+                    }]
+                }
+            });
+            chords.push({
+                'name': 'Db', 
+                'french_name': 'RÉ b',
+                'chords': {
+                    'Major': [{
+                        'Fingers': {
+                            'G1': '1',
+                            'C1': '1',
+                            'E1': '1',
+                            'A4': '4',
+                        }
+                    }]
+                }
+            });
+            chords.push({
+                'name': 'D', 
+                'french_name': 'RÉ',
+                'chords': {
+                    'Major': [{
+                        'Fingers': {
+                            'G2': '1',
+                            'C2': '2',
+                            'E2': '3',
+                        }
+                    }]
+                }
+            });
+            chords.push({
+                'name': 'Eb', 
+                'french_name': 'MI b',
+                'chords': {
+                    'Major': [{
+                        'Fingers': {
+                            'A1': '1',
+                            'C3': '2',
+                            'E3': '3',
+                        }
+                    }]
+                }
+            });
+            chords.push({
+                'name': 'E', 
+                'french_name': 'MI',
+                'chords': {
+                    'Major': [{
+                        'Fingers': {
+                            'A1': '1',
+                            'G4': '2',
+                            'C4': '3',
+                            'E4': '4',
+                        }
+                    }]
+                }
+            });
+            chords.push({
+                'name': 'F', 
+                'french_name': 'Fa',
+                'chords': {
+                    'Major': [{
+                        'Fingers': {
+                            'E1': '1',
+                            'G2': '2',
+                        }
+                    }]
+                }
+            });
+            chords.push({
+                'name': 'Gb', 
+                'french_name': 'SOL b',
+                'chords': {
+                    'Major': [{
+                        'Fingers': {
+                            'C1': '1',
+                            'A1': '1',
+                            'E2': '2',
+                            'G3': '3',
+                        }
+                    }]
+                }
+            });
+            chords.push({
+                'name': 'G', 
+                'french_name': 'Sol',
+                'chords': {
+                    'Major': [{
+                        'Fingers': {
+                            'C2': '1',
+                            'A2': '2',
+                            'E3': '3',
+                        }
+                    }]
+                }
+            });
+            chords.push({
+                'name': 'Ab', 
+                'french_name': 'LA b',
+                'chords': {
+                    'Major': [{
+                        'Position': 'middle',
+                        'Fingers': {
+                            'C2': '1',
+                            'A2': '2',
+                            'E3': '3',
+                        }
+                    }]
+                }
+            });
 
             return chords;
         }
