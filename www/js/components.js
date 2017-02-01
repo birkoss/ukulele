dfe2aba94efd0e8d63a43dcb0d8667326ee008d0
birkoss <admin@birkoss.com>
2017-02-01 14:47:30 -0500

    Adding a new components

---
+++ www/js/components.js
@@ -0,0 +1,9 @@
+ukuleleApp.component('chordFinger', {
+    bindings: {
+        fingers:'=fingers',
+        position:'=position',
+        labels:'=labels',
+    },
+
+    templateUrl:'chord-finger.html'
+});
