3621f1625290a3b4071e35f877f38740139487f6
birkoss <admin@birkoss.com>
2017-02-05 09:15:50 -0500

    Adding an interactive attribute to enable it only on demand

--- www/js/components.js
+++ www/js/components.js
@@ -3,42 +3,44 @@ ukuleleApp.component('chordFinger', {
         fingers:'=fingers',
         position:'=position',
         labels:'=labels',
+        interactive:'=interactive',
         muted:'=muted',
     },
     controller: function() {
         this.$onInit = function() {
-            console.log('onInit');
             this.toggleMarker = function(event) {
-                for (var i=0; i<event.path.length; i++) {
-                    if (event.path[i].nodeName == 'svg') {
-                        svg_parent = event.path[i];
+                if (this.interactive) {
+                    for (var i=0; i<event.path.length; i++) {
+                        if (event.path[i].nodeName == 'svg') {
+                            svg_parent = event.path[i];
+                        }
                     }
-                }
 
-                var x = event.offsetX;
-                var y = event.offsetY;
-                var parent_width = svg_parent.clientWidth;
-                var parent_height = svg_parent.clientHeight;
+                    var x = event.offsetX;
+                    var y = event.offsetY;
+                    var parent_width = svg_parent.clientWidth;
+                    var parent_height = svg_parent.clientHeight;
 
-                var scale = (parent_width / 250);
+                    var scale = (parent_width / 250);
 
-                var origin_start = 36 * scale;
-                var grid_size = 55 * scale;
+                    var origin_start = 36 * scale;
+                    var grid_size = 55 * scale;
 
-                x-= origin_start;
-                y-= origin_start;
+                    x-= origin_start;
+                    y-= origin_start;
 
-                var p_x = Math.min(3, Math.max(0, Math.round(x / grid_size)));
-                var p_y = Math.min(3, Math.max(0, Math.floor(y / grid_size)));
+                    var p_x = Math.min(3, Math.max(0, Math.round(x / grid_size)));
+                    var p_y = Math.min(3, Math.max(0, Math.floor(y / grid_size)));
 
-                var strings = {'0':'G', '1':'C', '2':'E', '3':'A'};
+                    var strings = {'0':'G', '1':'C', '2':'E', '3':'A'};
 
-                var fret_id = strings[p_x] + (p_y+1);
+                    var fret_id = strings[p_x] + (p_y+1);
 
-                if (this.fingers[fret_id]) {
-                    delete this.fingers[fret_id];
-                } else {
-                    this.fingers[fret_id] = '0';
+                    if (this.fingers[fret_id]) {
+                        delete this.fingers[fret_id];
+                    } else {
+                        this.fingers[fret_id] = '0';
+                    }
                 }
             };
         };
